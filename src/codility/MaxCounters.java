78bb4e41cb561d2edb8e7c9a3d0f5c9d5651058e
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-07 10:06:35 +0900

    100% : performance resolved!

--- src/codility/MaxCounters.java
+++ src/codility/MaxCounters.java
@@ -8,19 +8,21 @@ public class MaxCounters {
     public static void main(String[] args) {
         int[] A =
             //{ 3, 4, 4, 6, 1, 4, 4 }
-            { 3, 4, 4, 6, 1, 4, 4, 6 }
+            //{ 3, 4, 4, 6, 1, 4, 4, 6 }
+            //{ 3, 3, 3, 3, 3 }
             //{ 3, 3, 3, 3, 3 }
             //{ 1, 2, 3, 5, 4, 5 }
-            //{1, 3, 1, 3, 2, 1, 3}
-            //{1, 3, 2}
+            {1, 3, 4, 3, 2, 1, 3}
+//            {1, 3, 2}
             ;
         int N =
             //5
-            5
-            //2
             //5
+//            2
             //3
-            //3
+            //5
+            3
+//            3
             ;
         Object solution = new Solution().solution(N, A);
         TestHelper.printSolution(solution);
@@ -28,22 +30,35 @@ public class MaxCounters {
 
     static class Solution {
         public int[] solution(int N, int[] A) {
-            int[] counters = new int[N];
             final int MAX_COUNTER = N + 1;
+            int[] counters = new int[N];
             int currentMax = 0;
             boolean maxCounterOn = false;
+            int currentMaxCounter = 0;
 
             for (int opValue : A) {
                 if (opValue == MAX_COUNTER) {
                     if (!maxCounterOn) {
-                        Arrays.fill(counters, currentMax);
                         maxCounterOn = true;
+                        currentMaxCounter = currentMax;
                     }
                 } else {
-                    maxCounterOn = false;
-                    counters[opValue - 1] += 1;
+                    if (maxCounterOn) {
+                        counters[opValue - 1] = currentMaxCounter + 1;
+                        maxCounterOn = false;
+                    } else {
+                        if (counters[opValue - 1] <= currentMaxCounter) {
+                            counters[opValue - 1] = currentMaxCounter + 1;
+                        } else {
+                            counters[opValue - 1] += 1;
+                        }
+                    }
                     currentMax = Math.max(counters[opValue - 1], currentMax);
                 }
+            }
+
+            for (int i = 0; i < counters.length; i++) {
+                counters[i] = Math.max(counters[i], currentMaxCounter);
             }
 
             return counters;
