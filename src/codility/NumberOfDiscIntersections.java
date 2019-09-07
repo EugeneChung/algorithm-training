7c670ebbf4e31a106b2f3eeebc5a2f065333c05e
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-08 00:12:34 +0900

    100% - binarySearch to +1 was wrong - Because of it, A.length - 1 was the wrong patch.
    https://nukeguys.tistory.com/179
    https://rafal.io/posts/codility-intersecting-discs.html

--- src/codility/NumberOfDiscIntersections.java
+++ src/codility/NumberOfDiscIntersections.java
@@ -23,22 +23,24 @@ public class NumberOfDiscIntersections {
             if (A.length < 2) return 0;
 
             long[] L = new long[A.length];
+            long[] R = new long[A.length];
             for (int i = 0; i < A.length; i++) {
                 long radius = A[i];
                 L[i] = i - radius;
+                R[i] = i + radius;
             }
             Arrays.sort(L);
-            //TestHelper.printArray(Arrays.toString(L));
 
             long answer = 0;
-            for (int i = 0; i < A.length - 1; i++) {
-                long radius = A[i];
-                long R = i + radius;
-                long count = Arrays.binarySearch(L, R + 1L);
+            for (int i = 0; i < A.length; i++) {
+                long count = Arrays.binarySearch(L, R[i]);
                 if (count < 0) {
                     count = -count - 1L;
+                } else {
+                    while (count < A.length && L[(int)count] == R[i]) {
+                        count++;
+                    }
                 }
-                TestHelper.log(count);
                 answer += count - 1L - (long)i;
             }
             if (answer > 10000000) {
