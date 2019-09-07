3cf87f3f765d0a517561924ab9cf67afe68994c0
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-07 21:12:38 +0900

    100%

---
+++ src/codility/Nesting.java
@@ -0,0 +1,43 @@
+package codility;
+
+import helpers.TestHelper;
+
+import java.util.Stack;
+
+public class Nesting {
+    public static void main(String[] args) {
+        //"(", "{", "[", "]", "}" and/or ")"
+        String S =
+//            ""
+//            "("
+//            "()"
+            "((()"
+//            "(()(())())"
+//            "())"
+            ;
+        Object solution = new Solution().solution(S);
+        TestHelper.printSolution(solution);
+    }
+
+    static class Solution {
+        public int solution(String S) {
+            if (S.isEmpty()) return 1;
+            if (S.length() % 2 == 1) return 0;
+
+            Stack<Character> brackets = new Stack<>();
+            brackets.push(S.charAt(0));
+
+            for (int i = 1; i < S.length(); i++) {
+                char bracket = S.charAt(i);
+                if (bracket == '(') {
+                    brackets.push(bracket);
+                } else {
+                    if (brackets.isEmpty()) return 0;
+                    brackets.pop();
+                }
+            }
+            if (!brackets.isEmpty()) return 0;
+            return 1;
+        }
+    }
+}
