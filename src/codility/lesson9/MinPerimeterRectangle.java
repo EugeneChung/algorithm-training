d6c3e001b4f2563689a24f7802b07a33b4987abd
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-08 21:08:54 +0900

    100% O(Root N) - just a math..

---
+++ src/codility/lesson9/MinPerimeterRectangle.java
@@ -0,0 +1,33 @@
+package codility.lesson9;
+
+import helpers.TestHelper;
+
+public class MinPerimeterRectangle {
+    public static void main(String[] args) {
+        int N =
+            //1_000_000_000
+            4
+            ;
+        Object solution = new Solution().solution(N);
+        TestHelper.printSolution(solution);
+    }
+
+    static class Solution {
+        public int solution(long N) {
+            if (N == 1) return 4; // 2 * (1 + 1)
+
+            long minPerimeter = Integer.MAX_VALUE;
+            long i = 1;
+            while (i * i < N) {
+                if (N % i == 0) {
+                    minPerimeter = Math.min(minPerimeter, 2 * (i + N / i));
+                }
+                i++;
+            }
+            if (i * i == N) {
+                minPerimeter = Math.min(minPerimeter, 2 * (i + N / i));
+            }
+            return (int)minPerimeter;
+        }
+    }
+}
