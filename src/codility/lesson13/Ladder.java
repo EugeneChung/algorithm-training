5a39ebd1f1002e12345659db02c26c310c798b17
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-09 15:57:46 +0900

    75% DP : https://app.codility.com/demo/results/trainingNNRZ2U-DM5/

--- src/codility/lesson13/Ladder.java
+++ src/codility/lesson13/Ladder.java
@@ -11,16 +11,16 @@ import java.util.List;
 public class Ladder {
     public static void main(String[] args) {
         int[] A =
-//            {4, 4, 5, 5, 1}
+            {4, 4, 5, 5, 1}
 //            {3, 3}
 //            {100, 150}
-            {1, 1}
+//            {1, 1}
             ;
         int[] B =
-//            {3, 2, 4, 3, 1}
+            {3, 2, 4, 3, 1}
 //            {1, 30}
 //            {10, 30}
-            {1, 1}
+//            {1, 1}
             ;
 
         Object solution = new Solution().solution(A, B);
@@ -36,24 +36,28 @@ public class Ladder {
             }
         }
 
+        private int maxInArray(int... array) {
+            int max = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
         public int[] solution(int[] A, int[] B) {
-            //TestHelper.log(twoExponents);
             List<BigInteger> fiboLadders = new ArrayList<>(Arrays.asList(
                 BigInteger.valueOf(0L), BigInteger.valueOf(1L), BigInteger.valueOf(2L), BigInteger.valueOf(3L))
             );
-            int maxL = fiboLadders.size() - 1;
+            int maxL = maxInArray(A);
+            for (int i = 4; i <= maxL; i++) {
+                fiboLadders.add(fiboLadders.get(fiboLadders.size() - 2).add(fiboLadders.get(fiboLadders.size() - 1)));
+            }
+            TestHelper.log(fiboLadders);
 
             int[] answer = new int[A.length];
             for (int i = 0; i < A.length; i++) {
-                // update fiboLadders list
-                if (maxL < A[i]) {
-                    for (int j = 0; j < A[i] - maxL; j++) {
-                        fiboLadders.add(fiboLadders.get(fiboLadders.size() - 2).add(fiboLadders.get(fiboLadders.size() - 1)));
-                    }
-                    maxL = A[i];
-                }
-                //TestHelper.log(fiboLadders);
-
                 BigInteger count = fiboLadders.get(A[i]);
                 answer[i] = count.mod(twoExponents.get(B[i] - 1)).intValue();
             }
