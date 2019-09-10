7229a4b173d0f735ce0e9f5d237f9edb2f835b89
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-10 17:25:37 +0900

    100% : easy

---
+++ src/codility/lesson16/TiedRopes.java
@@ -0,0 +1,34 @@
+package codility.lesson16;
+
+import helpers.TestHelper;
+
+public class TiedRopes {
+    public static void main(String[] args) {
+        int K =
+//            4
+            4
+            ;
+        int[] A =
+//            {1, 2, 3, 4, 1, 1, 3}
+            {1}
+            ;
+        Object solution = new Solution().solution(K, A);
+        TestHelper.printSolution(solution);
+    }
+
+    //https://app.codility.com/demo/results/trainingQKU7U3-6X3/
+    static class Solution {
+        public int solution(int K, int[] A) {
+            int tiedRopesCount = 0;
+            int currentTiedRopeLen = 0;
+            for (int i = 0; i < A.length; i++) {
+                currentTiedRopeLen += A[i];
+                if (currentTiedRopeLen >= K) {
+                    tiedRopesCount++;
+                    currentTiedRopeLen = 0;
+                }
+            }
+            return tiedRopesCount;
+        }
+    }
+}
