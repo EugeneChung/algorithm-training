70e9e97043fc96ff6de1af6abfbaa13be6ffc277
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-11 16:16:55 +0900

    another 100% : https://app.codility.com/demo/results/trainingUWU22Q-NZH/ rewritten for review

--- src/codility/lesson4/MaxCounters.java
+++ src/codility/lesson4/MaxCounters.java
@@ -7,22 +7,24 @@ import java.util.Arrays;
 public class MaxCounters {
     public static void main(String[] args) {
         int[] A =
-            //{ 3, 4, 4, 6, 1, 4, 4 }
-            //{ 3, 4, 4, 6, 1, 4, 4, 6 }
-            //{ 3, 3, 3, 3, 3 }
-            //{ 3, 3, 3, 3, 3 }
-            //{ 1, 2, 3, 5, 4, 5 }
-            {1, 3, 4, 3, 2, 1, 3}
-//            {1, 3, 2}
+//            { 3, 4, 4, 6, 1, 4, 4 }
+//            { 3, 4, 4, 6, 1, 4, 4, 6 }
+//            { 3, 3, 3, 3, 3 }
+//            { 3, 3, 3, 3, 3 }
+//            { 3, 3, 3, 3, 3, 4 }
+//            { 1, 2, 3, 5, 4, 5 }
+//            {1, 3, 4, 3, 2, 1, 3}
+            {1, 3, 2}
             ;
         int N =
-            //5
-            //5
-//            2
-            //3
-            //5
+//            5 //[3, 2, 2, 4, 2]
+//            5 //[4, 4, 4, 4, 4]
+//            2 //[0, 0]
+//            3 //[0, 0, 5]
+//            3 //[5, 5, 5]
+//            5 //[1, 1, 1, 1, 2]
+//            3 //[2, 2, 3]
             3
-//            3
             ;
         Object solution = new Solution().solution(N, A);
         TestHelper.printSolution(solution);
@@ -30,37 +32,30 @@ public class MaxCounters {
 
     static class Solution {
         public int[] solution(int N, int[] A) {
-            final int MAX_COUNTER = N + 1;
+            final int maxCounterCommand = N + 1;
             int[] counters = new int[N];
             int currentMax = 0;
-            boolean maxCounterOn = false;
             int currentMaxCounter = 0;
 
-            for (int opValue : A) {
-                if (opValue == MAX_COUNTER) {
-                    if (!maxCounterOn) {
-                        maxCounterOn = true;
-                        currentMaxCounter = currentMax;
-                    }
+            for (int operation : A) {
+                if (operation == maxCounterCommand) {
+                    currentMaxCounter = currentMax;
                 } else {
-                    if (maxCounterOn) {
-                        counters[opValue - 1] = currentMaxCounter + 1;
-                        maxCounterOn = false;
-                    } else {
-                        if (counters[opValue - 1] <= currentMaxCounter) {
-                            counters[opValue - 1] = currentMaxCounter + 1;
-                        } else {
-                            counters[opValue - 1] += 1;
-                        }
+                    //max counter 고려
+                    int c = counters[operation - 1];
+                    if (c < currentMaxCounter) {
+                        c = currentMaxCounter;
                     }
-                    currentMax = Math.max(counters[opValue - 1], currentMax);
+                    c = c + 1;
+                    counters[operation - 1] = c;
+                    currentMax = Math.max(currentMax, c);
                 }
             }
-
-            for (int i = 0; i < counters.length; i++) {
-                counters[i] = Math.max(counters[i], currentMaxCounter);
+            for (int i = 0; i < N; i++) {
+                if (counters[i] < currentMaxCounter) {
+                    counters[i] = currentMaxCounter;
+                }
             }
-
             return counters;
         }
     }
