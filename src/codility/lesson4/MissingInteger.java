79743bcec3bf61da9a2ce0b78b77f31dbc889174
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-11 16:28:55 +0900

    another 100%; don't use stream for coding test because of performance degrade (copy overhead or invokedynamic overhead)

--- src/codility/lesson4/MissingInteger.java
+++ src/codility/lesson4/MissingInteger.java
@@ -10,11 +10,18 @@ import java.util.stream.IntStream;
 public class MissingInteger {
     public static void main(String[] args) {
         int[] A =
-            //{ 1, 3, 6, 4, 1, 2 }
-            { 1, 2, 3 }
-            //{ -1, -3 }
-            //{1, 3, 1, 3, 2, 1, 3}
-            //{1, 3, 2}
+//            { 1, 3, 6, 4, 1, 2 } //5
+//            { 1, 2, 3 } // 4
+//            { -1, -3 } // 1
+//            { 10, 57, 5 } // 1
+//            { 10, 1, 57, 5 } // 2
+//            { 10, 3, 2 } // 1
+//            {1, 3, 1, 3, 2, 1, 3} // 4
+//            {1, 1, 1, 1, 1, 1} //2
+//            {-1, 3, 1, -1, 1, 0, 1, 5} //2
+//            {1} // 2
+//            {-1} // 1
+            {0} // 1
             ;
         Object solution = new Solution().solution(A);
         TestHelper.printSolution(solution);
@@ -22,21 +29,21 @@ public class MissingInteger {
 
     static class Solution {
         public int solution(int[] A) {
-            int[] intList = IntStream.of(A).filter(e -> e > 0).sorted().toArray();
-            int prev = 0;
+            Arrays.sort(A);
+
+            if (A[A.length - 1] <= 0) return 1;
             int answer = 1;
+            int prev = Integer.MIN_VALUE;
+            for (int value : A) {
+                if (value <= 0) continue;
+                if (prev == value) continue;
 
-            for (final int curr : intList) {
-                if (prev != curr) {
-                    if (curr != answer) {
-                        break;
-                    } else {
-                        answer += 1;
-                        prev = curr;
-                    }
+                if (answer != value) {
+                    return answer;
                 }
+                prev = value;
+                answer++;
             }
-
             return answer;
         }
     }
