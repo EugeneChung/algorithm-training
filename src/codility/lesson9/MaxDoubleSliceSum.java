572b761d355b275082b9b8537963a9d6d6ee161b
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-13 00:41:41 +0900

    69% : https://app.codility.com/demo/results/trainingMKG9G8-EAP/ With the help of https://rafal.io/posts/codility-max-double-slice-sum.html, but I don't understand why the perf problem exists.

--- src/codility/lesson9/MaxDoubleSliceSum.java
+++ src/codility/lesson9/MaxDoubleSliceSum.java
@@ -2,6 +2,9 @@ package codility.lesson9;
 
 import helpers.TestHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaxDoubleSliceSum {
     public static void main(String[] args) {
         int [] A =
@@ -16,19 +19,34 @@ public class MaxDoubleSliceSum {
             if (A.length == 3) return 0;
 
             int maxEndingLeft = 0;
-            int maxSoFarLeft = 0;
             int maxEndingRight = 0;
-            int maxSoFarRight = 0;
 
-            for (int x = 0; x < A.length - 2; x++) {
-                for (int y = x + 1; y < A.length - 1; y++) {
-                    for (int z = y + 1; z < A.length; z++) {
-                        maxEndingRight = Math.max(A[x], A[x] + maxEndingRight);
-                        //maxSoFar = Math.max(maxSoFar, maxEnding);
-                    }
-                }
+            List<Integer> maxEndingFromLeftList = new ArrayList<>(A.length);
+            List<Integer> maxEndingFromRightList = new ArrayList<>(A.length);
+
+            maxEndingFromLeftList.add(0); // for empty slice (X=0, Y=1)
+            for (int i = 1; i < A.length - 1; i++) {
+                maxEndingLeft = Math.max(0, maxEndingLeft + A[i]);
+                maxEndingFromLeftList.add(maxEndingLeft);
             }
-            return 0;
+            maxEndingFromLeftList.add(0); // for empty slice (Y=N-2, Z=N-1)
+
+            maxEndingFromRightList.add(0, 0);
+            for (int j = A.length - 2; j > 0; j--) {
+                maxEndingRight = Math.max(0, maxEndingRight + A[j]);
+                maxEndingFromRightList.add(0, maxEndingRight);
+            }
+            maxEndingFromRightList.add(0, 0);
+//            TestHelper.log(maxEndingFromLeftList);
+//            TestHelper.log(maxEndingFromRightList);
+
+            int answer = Integer.MIN_VALUE;
+            for (int i = 1; i < maxEndingFromLeftList.size() - 1; i++) {
+                int leftSum = maxEndingFromLeftList.get(i - 1);
+                int rightSum = maxEndingFromRightList.get(i + 1);
+                answer = Math.max(answer, leftSum + rightSum);
+            }
+            return answer;
         }
     }
 }
