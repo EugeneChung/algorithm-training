8282fca14e1ae88f4a1cb00429233e8d707e467f
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-12 17:40:01 +0900

    100% : my own solution with HashSet https://app.codility.com/demo/results/trainingABDW6V-WBE/ FYI, with LinkedHashSet you could debug easily.

--- src/codility/lesson15/CountDistinctSlice.java
+++ src/codility/lesson15/CountDistinctSlice.java
@@ -10,12 +10,12 @@ public class CountDistinctSlice {
             6
             ;
         int[] A =
-//            {3, 4, 5, 5, 2}
-            {1, 2, 3, 4, 1, 3, 4}
-//            {100, 150}
-//            {0, 1}
-//            {0, 1, 0}
-//            {3, 2, 1}
+//            {3, 4, 5, 5, 2} // 9
+//            {3, 4, 5, 6, 6, 2} // 13
+//            {1, 2, 3, 4, 1, 3, 4} // 20 
+//            {100, 150} // 3
+//            {0, 1, 0} // 5
+            {3, 2, 1} // 5
             ;
         Object solution = new Solution().solution(M, A);
         TestHelper.printSolution(solution);
@@ -24,16 +24,34 @@ public class CountDistinctSlice {
     static class Solution {
         public int solution(int M, int[] A) {
             if (A.length == 1) return 1;
-            int[] nextPositions = new int[M + 1];
-            int left = 0;
-            int answer = 0;
 
-            for (int right = 0; right < A.length; right++) {
-                if (nextPositions[A[right]] > left) {
-                    left = nextPositions[A[right]];
+            int left = 0;
+            int right = 0;
+            int answer = 1;
+            Set<Integer> uniqueSlice = new HashSet<>(Collections.singleton(A[0]));
+
+            while (right < A.length) {
+                if (right == A.length - 1 || !uniqueSlice.add(A[right + 1])) {
+                    if (left == right) {
+                        uniqueSlice.remove(A[left]);
+                        left++;
+                        right++;
+                        if (right < A.length) {
+                            uniqueSlice.add(A[right]);
+                        } else if (uniqueSlice.isEmpty()) {
+                            break;
+                        }
+                        answer++;
+                    } else {
+                        uniqueSlice.remove(A[left]);
+                        left++;
+                        answer += right - left + 1;
+                    }
+                } else {
+                    right++;
+                    answer++;
                 }
-                nextPositions[A[right]] = right + 1;
-                answer += right - left + 1;
+//                TestHelper.log(uniqueSlice + " count=" + answer);
                 if (answer > 1_000_000_000) return 1_000_000_000;
             }
             return answer;
