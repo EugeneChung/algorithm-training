dd7ad8fd91f347056f21c5e6d055fed1aa11a51b
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-13 11:44:51 +0900

    100% : https://app.codility.com/demo/results/trainingBPEE7T-ZYK/ As explained by https://codility.com/media/train/13-CaterpillarMethod.pdf exercise

--- src/codility/lesson15/CountTriangle.java
+++ src/codility/lesson15/CountTriangle.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class CountTriangle {
     public static void main(String[] args) {
         int[] A =
-            {10, 2, 5, 1, 8, 12, 11, 15} // 12
+            {10, 2, 5, 1, 8, 12, 11, 15} // 20
 //            {10, 2, 5, 1, 8, 12} // 4
 //            {3, 2, 1} // 0
 //            {3, 2, 7} // 0
@@ -27,48 +27,16 @@ public class CountTriangle {
 
             Arrays.sort(A);
 
-            int left = 0;
-            int right = 2;
             int answer = 0;
 
-            while (right < A.length) {
-                if (right - left >= 2 && A[left] + A[left + 1] > A[right]) {
-                    answer++;
-                }
-//                TestHelper.log("left=" + left + ", right=" + right + ", answer=" + answer);
-
-                if (right < A.length - 1) {
-                    // check to move right
-                    if (right == left) {
-                        if (right < A.length - 2) {
-                            if (A[right] + A[right + 1] > A[right + 2]) {
-                                right += 2;
-                            } else {
-                                answer += findTriangeWhileMovingLeft(A, left, right);
-                                left++;
-                            }
-                        } else {
-                            break;
-                        }
-                    } else if (right - left == 1) {
-                        if (A[left] + A[right] > A[right + 1]) {
-                            right++;
-                        } else {
-                            answer += findTriangeWhileMovingLeft(A, left, right);
-                            left++;
-                        }
-                    } else if (A[left] + A[left + 1] > A[right + 1]) {
+            for (int left = 0; left < A.length - 2; left++) {
+                int right = left + 2;
+                for (int mid = left + 1; mid < A.length - 1; mid++) {
+                    while (right < A.length && A[left] + A[mid] > A[right]) {
                         right++;
-                    } else {
-                        answer += findTriangeWhileMovingLeft(A, left, right);
-                        left++;
                     }
-                } else {
-                    answer += findTriangeWhileMovingLeft(A, left, right);
-                    left++;
-                    if (right - left < 2) {
-                        break;
-                    }
+                    answer += right - mid - 1;
+                    //TestHelper.log("left=" + left + ",mid=" + mid + ",right=" + right + ",count=" + answer);
                 }
             }
 
