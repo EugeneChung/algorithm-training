de06a079ea25224aab2d4a15ee0653d4e60e4fd5
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-14 23:19:37 +0900

    100% : use suffix sum https://app.codility.com/demo/results/trainingSHTUGJ-CKQ/

--- src/codility/lesson5/PassingCars.java
+++ src/codility/lesson5/PassingCars.java
@@ -2,21 +2,17 @@ package codility.lesson5;
 
 import helpers.TestHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class PassingCars {
     public static void main(String[] args) {
         int[] A =
-            //{ 0, 1, 0, 1, 1 }
-            //{ 1, 0, 1 }
-            //{ 0, 0, 0 }
-            //{ 1, 1, 1 }
-            //{ 1 }
-//            { 0 }
-//            { 1, 0, 0 }
-            { 0, 0, 1 }
+            { 0, 1, 0, 1, 1 }
+//            { 1, 0, 1 } //1
+//            { 0, 0, 0 }//0
+//            { 1, 1, 1 } //0
+//            { 1 } // 0
+//            { 0 } // 0
+//            { 1, 0, 0 } // 0
+//            { 0, 0, 1 } // 2
             ;
         Object solution = new Solution().solution(A);
         TestHelper.printSolution(solution);
@@ -24,13 +20,19 @@ public class PassingCars {
 
     static class Solution {
         public int solution(int[] A) {
-            int eastCarCount = 0;
+            if (A.length == 1) return 0;
+            int[] suffixSum = new int[A.length + 1];
+
+            for (int i = A.length - 1; i >= 0; i--) {
+                suffixSum[i] = suffixSum[i + 1] + A[i];
+            }
+//            TestHelper.printObject(suffixSum);
+
             int pairs = 0;
-            for (int carDirection : A) {
+            for (int i = 0; i < A.length; i++) {
+                int carDirection = A[i];
                 if (carDirection == 0) {
-                    eastCarCount += 1;
-                } else {
-                    pairs += eastCarCount;
+                    pairs += suffixSum[i];
                     if (pairs > 1000000000) {
                         return -1;
                     }
