b7f8200018015f8efb7107294ec15c8a5ab0d1a8
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-14 16:03:35 +0900

    69%: https://app.codility.com/demo/results/trainingJDWKQZ-BMV/ correctness OK

---
+++ src/codility/lesson90/RectangularArea.java
@@ -0,0 +1,71 @@
+package codility.lesson90;
+
+import helpers.TestHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RectangularArea {
+    public static void main(String[] args) {
+        int[] A = {1, 2, 5, 1, 1, 7, 2, 3, 5, 1, 7}; int X = 1; // 7
+//        int[] A = {1_000_000_000, 1_000_000_000, 1_000_000_000, 1_000_000_000}; int X = Integer.MAX_VALUE; // 1
+//        int[] A = {1, 1, 1, 2, 2, 1}; int X = 1; // 2
+//        int[] A = {1, 1, 1, 2, 1}; int X = 1; // 1
+//        int[] A = {1, 2, 5, 1, 1, 2, 3, 5, 1}; int X = 5; // 2
+        Object solution = new Solution().solution(A, X);
+        TestHelper.printSolution(solution);
+    }
+
+    static class Solution {
+        public int solution(int[] A, int X) {
+            if (A.length < 4) return 0;
+
+            Arrays.sort(A);
+            List<Integer> lengths = new ArrayList<>();
+            List<Integer> lengthOver4s = new ArrayList<>();
+            int occur = 0;
+            int prev = Integer.MAX_VALUE;
+            for (int value : A) {
+                if (prev != value) {
+                    prev = value;
+                    occur = 1;
+                } else {
+                    occur++;
+                    if (occur == 2) {
+                        lengths.add(prev);
+                    } else if (occur == 4) {
+                        lengthOver4s.add(prev);
+                    }
+                }
+            }
+            TestHelper.log(lengthOver4s);
+            TestHelper.log(lengths);
+
+            int count = 0;
+            for (long len : lengthOver4s) {
+                long area = len * len;
+                if (area >= X) {
+                    count++;
+                    if (count > 1_000_000_000) {
+                        return -1;
+                    }
+                }
+            }
+            for (int i = 0; i < lengths.size(); i++) {
+                for (int j = i + 1; j < lengths.size(); j++) {
+                    long height = lengths.get(i);
+                    long width = lengths.get(j);
+                    long area = height * width;
+                    if (area >= X) {
+                        count++;
+                        if (count > 1_000_000_000) {
+                            return -1;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
