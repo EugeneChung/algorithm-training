dc8573b3bea459de79ed7c1e093e4e2365ff290a
Eugene Chung <bluewolf.chung@gmail.com>
2019-09-16 16:09:50 +0900

    https://programmers.co.kr/learn/courses/30/parts/12081

---
+++ src/programmers/dfsbfs/TargetNumber.java
@@ -0,0 +1,30 @@
+package programmers.dfsbfs;
+
+import helpers.TestHelper;
+
+public class TargetNumber {
+    public static void main(String[] args) {
+        int[] lost = {1, 1, 1, 1, 1}; int target = 3;
+
+        TestHelper.printSolution(new Solution().solution(lost, target));
+    }
+
+    static class Solution {
+        public int solution(int[] numbers, int target) {
+            return traverseNumbers(numbers, target, 0, 0);
+        }
+
+        private int traverseNumbers(final int[] numbers, final int target, int index, int sum) {
+            if (index == numbers.length) {
+                if (sum == target) {
+                    return 1;
+                }
+                return 0;
+            } else {
+                int number = numbers[index];
+                return traverseNumbers(numbers, target, index + 1, sum + number) +
+                    traverseNumbers(numbers, target, index + 1, sum - number);
+            }
+        }
+    }
+}
--- src/programmers/stackqueue/Printer.java
+++ src/programmers/stackqueue/Printer.java
@@ -1,13 +1,19 @@
-package programmers;
+package programmers.stackqueue;
 
-public class TargetNumber {
+import helpers.TestHelper;
+
+public class Printer {
     public static void main(String[] args) {
-        int[] lost = {1, 1, 1, 1, 1};
-        int reserve = 3;
-        System.out.println(new Solution().solution(lost, reserve));
+        int[] lost = {1, 1, 1, 1, 1}; int target = 3;
+
+        TestHelper.printSolution(new Solution().solution(lost, target));
     }
 
     static class Solution {
+        public int solution(int[] numbers, int target) {
+            return traverseNumbers(numbers, target, 0, 0);
+        }
+
         private int traverseNumbers(final int[] numbers, final int target, int index, int sum) {
             if (index == numbers.length) {
                 if (sum == target) {
@@ -19,10 +25,6 @@ public class TargetNumber {
                 return traverseNumbers(numbers, target, index + 1, sum + number) +
                     traverseNumbers(numbers, target, index + 1, sum - number);
             }
-        }
-
-        public int solution(int[] numbers, int target) {
-            return traverseNumbers(numbers, target, 0, 0);
         }
     }
 }
